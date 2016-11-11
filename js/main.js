e92a1fc101c652ca2fd73ca0f5870a34f1d7e28b
Ali Camarata <alisalaah@gmail.com>
2016-11-11 21:25:50 +0700

    theme and starter posts

---
+++ js/main.js
@@ -0,0 +1,9 @@
+---
+---
+{% include js/bootstrap.js %}
+{% include js/slick.js %}
+{% include js/waypoints.js %}
+{% include js/jquery.counterup.js %}
+{% include js/wow.js %}
+{% include js/bootstrap-progressbar.js %}
+{% include js/custom.js %}
